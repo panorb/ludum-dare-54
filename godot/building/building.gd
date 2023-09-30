99f1e48a5bbcfa1b6632199e98e19e7ac546b61c
Julian Pape-Lange <j.pape-lange@gmx.de>
2023-10-01 00:07:54 +0200

    Fix slopes

--- godot/building/building.gd
+++ godot/building/building.gd
@@ -6,9 +6,9 @@ class_name Building extends Node
 @export var max_width : int = 10
 @export var floor_width_distribution = [0, 0, 0.1, 0.25, 0.4, 0.55, 0.7, 0.8, 0.9, 0.96, 1]
 @export var floor_number_distribution = [0, 0.3, 0.4, 1]
-@export var border_probability = 0.3
+@export var border_probability = 1
 @export var left_border_probability = 0.5
-@export var adjusted_expanding_distribution = [0.02, 0.22, 0.68, 0.98, 1]
+@export var adjusted_expanding_distribution = [10.05, 0.22, 0.68, 0.98, 1]
 @export var no_tshape_probability = 0.2
 
 var grid = []
@@ -21,68 +21,70 @@ func generate_building():
 	var is_border = randomize_border()
 	var is_left_border_if_border = randomize_left_border()
 	
-	var lower_size = randomize_size()
+	var lower_width = randomize_size()
 	
-	var upper_size = randomize_size()
+	var upper_width = randomize_size()
 	if (floor_number == 1):
-		upper_size = lower_size
+		upper_width = lower_width
 	
-	var offset = randomize_offset(lower_size, upper_size, is_border, is_left_border_if_border)
+	var max_width = max(lower_width, upper_width)
 	
-	var expand_size = 0
+	var offset = randomize_offset(lower_width, upper_width, is_border, is_left_border_if_border)
+	
+	var shrink_size = 0
 	if is_border:
-		expand_size = randomize_expand_size(lower_size, upper_size)
+		shrink_size = randomize_shrink_size(lower_width, upper_width)
 	
 	for i in floor_height * floor_number:
 		grid.append([])
-		for j in max(lower_size, upper_size):
+		for j in max_width:
 			var tile_instance = Building_Tile.new()
 			grid[i].append(tile_instance)
 
 	for i in floor_height * upper_height:
-		for j in upper_size:
+		for j in upper_width:
 			grid[i][-offset+j].set_brick()
 	
 	for i in floor_height * (floor_number - upper_height):
-		for j in lower_size:
+		for j in lower_width:
 			grid[i+floor_height*upper_height][offset+j].set_brick()
 	
 	if is_border:
-		if expand_size > 0:
+		if shrink_size > 0:
 			if is_left_border_if_border:
-				for i in expand_size:
-					for j in expand_size - i:
+				for i in shrink_size:
+					for j in shrink_size - i:
 						grid[i][j].unset_brick()
-						if i+j == expand_size - 1:
+						if i+j == shrink_size - 1:
 							grid[i][j].set_slope(1)
 			else:
-				for i in expand_size:
-					for j in expand_size - i:
-						grid[i][upper_size-1-j].unset_brick()
-						if i+j == expand_size - 1:
-							grid[i][upper_size-1-j].set_slope(2)
-		if expand_size < 0:
+				for i in shrink_size:
+					for j in shrink_size - i:
+						grid[i][max_width-1-j].unset_brick()
+						if i+j == shrink_size - 1:
+							grid[i][max_width-1-j].set_slope(2)
+		if shrink_size < 0:
 			if is_left_border_if_border:
-				for i in expand_size:
-					for j in expand_size - i:
+				for i in -shrink_size:
+					for j in -shrink_size - i:
 						grid[floor_height*floor_number-1-i][j].unset_brick()
-						if i+j == expand_size - 1:
+						if i+j == -shrink_size - 1:
 							grid[floor_height*floor_number-1-i][j].set_slope(3)
 			else:
-				for i in expand_size:
-					for j in expand_size - i:
-						grid[floor_height*floor_number-1-i][upper_size-1-j].unset_brick()
-						if i+j == expand_size - 1:
-							grid[floor_height*floor_number-1-i][upper_size-1-j].set_slope(4)
+				for i in -shrink_size:
+					for j in -shrink_size - i:
+						grid[floor_height*floor_number-1-i][max_width-1-j].unset_brick()
+						if i+j == -shrink_size - 1:
+							grid[floor_height*floor_number-1-i][max_width-1-j].set_slope(4)
 
-	for j in max(lower_size, upper_size):
+	for j in max_width:
 		if not grid[0][j].is_empty:
 			grid[0][j].set_top()
 		if floor_number > 1:
 			if (not grid[floor_height*upper_height][j].is_empty) and grid[floor_height*upper_height-1][j].is_empty:
 				grid[floor_height*upper_height][j].set_top()
 
-	for j in max(lower_size, upper_size):
+	for j in max_width:
 		if not grid[floor_number*floor_height-1][j].is_empty:
 			grid[floor_number*floor_height-1][j].set_bottom()
 		if floor_number > 1:
@@ -90,7 +92,7 @@ func generate_building():
 				grid[floor_height*upper_height-1][j].set_bottom()
 	
 	self.size.y = floor_number*floor_height
-	self.size.x = max(upper_size, lower_size)
+	self.size.x = max(upper_width, lower_width)
 
 func randomize_floor_number():
 	var random = randf()
@@ -118,29 +120,29 @@ func randomize_size():
 		floor_width += 1
 	return floor_width
 
-func randomize_offset(lower_size, upper_size, is_border, is_left_border_if_border):
+func randomize_offset(lower_width, upper_width, is_border, is_left_border_if_border):
 	if is_border:
 		if is_left_border_if_border:
 			return 0
 		else:
-			return upper_size-lower_size
+			return upper_width-lower_width
 	if randf() < no_tshape_probability:
 		if randf() < 0.5:
-			return max(upper_size-lower_size, 0)
+			return max(upper_width-lower_width, 0)
 		else:
-			return min(upper_size-lower_size, 0)
-	var max_offset = max(upper_size-lower_size, 0)
-	var min_offset = min(upper_size-lower_size, 0)
+			return min(upper_width-lower_width, 0)
+	var max_offset = max(upper_width-lower_width, 0)
+	var min_offset = min(upper_width-lower_width, 0)
 	return randi_range(min_offset, max_offset)
 
-func randomize_expand_size(lower_size, upper_size):
+func randomize_shrink_size(lower_width, upper_width):
 	var random = randf()
-	var adjusted_expand_size = 0
-	while adjusted_expanding_distribution[adjusted_expand_size] < random:
-		adjusted_expand_size += 1
-	var expand_size = adjusted_expand_size - 2
+	var adjusted_shrink_size = 0
+	while adjusted_expanding_distribution[adjusted_shrink_size] < random:
+		adjusted_shrink_size += 1
+	var shrink_size = adjusted_shrink_size - 2
 	
-	expand_size = max(expand_size, min_width - lower_size, min_width - upper_size)
-	expand_size = min(expand_size, lower_size - min_width, upper_size - min_width)
+	shrink_size = max(shrink_size, min_width - lower_width, min_width - upper_width)
+	shrink_size = min(shrink_size, lower_width - min_width, upper_width - min_width)
 	
-	return expand_size
+	return shrink_size
