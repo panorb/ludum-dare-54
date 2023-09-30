14a0e201b165cbf1038132b518bd846e2ac2d560
Leonard Paul Neumann <leonard-paul.neumann@s2020.tu-chemnitz.de>
2023-09-30 15:08:08 +0200

    Add first draft main menu

---
+++ godot/gui/QuitButton.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _pressed():#Schließt das Spiel
+	get_tree().quit()
---
+++ godot/gui/SoundSlider.gd
@@ -0,0 +1,7 @@
+extends HSlider
+
+
+# Called when the node enters the scene tree for the first time.
+func _value_changed():
+	
+	
