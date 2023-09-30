38524e8d9feabc0b9f18ff4e209ae8d8863cf3e7
Paul Norberger <panor@fastmail.com>
2023-09-30 23:00:11 +0200

    Card hand now properly centered and recentered after select

--- godot/game/cards/card.gd
+++ godot/game/cards/card.gd
@@ -2,11 +2,46 @@ class_name Card
 extends Sprite2D
 
 @onready var capacity_label : Label = get_node("%CapacityLabel")
+@onready var click_zone : Area2D = get_node("ClickZone")
+
+signal hover_begin(card : Card)
+signal hover_end(card : Card)
+signal selected(card : Card)
 
 func _ready():
+	self.click_zone.mouse_entered.connect(self._on_ClickZone_mouse_entered)
+	self.click_zone.mouse_exited.connect(self._on_ClickZone_mouse_exited)
+	self.click_zone.input_event.connect(self._on_ClickZone_input_event)
 	init(5)
 
 func init(capacity : int):
 	capacity_label.text = str(capacity)
 
+func _on_ClickZone_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if event is InputEventMouseButton and event.is_pressed():
+		self.selected.emit(self)
 
+func _on_ClickZone_mouse_entered():
+	self.hover_begin.emit(self)
+
+func _on_ClickZone_mouse_exited():
+	self.hover_end.emit(self)
+
+var focus_tween : Tween
+
+func focus():
+	if focus_tween:
+		focus_tween.stop()
+		focus_tween.kill()
+	focus_tween = create_tween()
+	focus_tween.set_trans(Tween.TRANS_EXPO)
+	focus_tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.2)
+	focus_tween.play()
+
+func blur():
+	if focus_tween:
+		focus_tween.stop()
+		focus_tween.kill()
+	focus_tween = create_tween()
+	focus_tween.tween_property(self, "scale", Vector2.ONE, 0.4)
+	focus_tween.play()
--- godot/game/cards/card_hand.gd
+++ godot/game/cards/card_hand.gd
@@ -3,52 +3,81 @@ extends Node2D
 signal card_hand_ready
 
 @export var card_scene : PackedScene = preload("res://game/cards/card.tscn")
-@export_range(5, 75) var card_spacing : int = 50
-@export_range(-75, 75) var y_offset : int = -50
+@export_range(5, 75) var card_spacing : int = 5
+@export_range(-75, 75) var y_offset : int = -45
+
+var hand_cards : Array[Card] = []
+var held_card : Card = null
 
 func _ready() -> void:
 	initial_card_draw()
 
 func initial_card_draw() -> void:
-	draw_card()
-	await self.get_tree().create_timer(1.0).timeout
-	draw_card()
-	await self.get_tree().create_timer(1.0).timeout
-	draw_card()
-	await self.get_tree().create_timer(0.4).timeout
+	for i in 12:
+		draw_card()
 	
 	reorganize_hand()
+	
 	self.card_hand_ready.emit()
 
 func draw_card() -> void:
 	print("Drawing a card...")
-	var card_num : int = get_child_count()
-	print(get_viewport_rect().size)
-	var y = get_viewport_rect().size.y + y_offset
-	var x = ((get_viewport_rect().size.x / 2) - 30) + (30 * card_num)
+	var y = get_viewport_rect().size.y + 120
+	var x = get_viewport_rect().size.x / 2
 	
 	var card_instance : Card = card_scene.instantiate()
-	#card_instance.position.x = x
-	#card_instance.position.y = y
+	card_instance.hover_begin.connect(self._on_Card_hover_begin)
+	card_instance.hover_end.connect(self._on_Card_hover_end)
+	card_instance.selected.connect(self._on_Card_selected)
+	card_instance.position.x = x
+	card_instance.position.y = y
 	
 	add_child(card_instance)
+	hand_cards.append(card_instance)
+
+func _on_Card_hover_begin(card : Card) -> void:
+	card.focus()
+
+func _on_Card_hover_end(card : Card) -> void:
+	card.blur()
+
+func _on_Card_selected(card : Card) -> void:
+	hand_cards = hand_cards.filter(func(elem : Card): return elem.name != card.name)
+	held_card = card
+	card.visible = false
+	
+	reorganize_hand()
 
 func reorganize_hand() -> void:
-	var card_num : int = get_child_count()
-	var start_x = (get_viewport_rect().size.x / 2) - ((card_num / 2) * card_spacing)
+	var card_num : int = len(hand_cards)
+	if not card_num:
+		return
+	
+	print("card_num: ", card_num)
+	
+	var spacer_num = card_num - 1
+	var total_width = card_num * hand_cards[0].texture.get_width()
+	total_width += spacer_num * card_spacing
+	var half_width = total_width / 2.0
+	
+	var start_x = (get_viewport_rect().size.x / 2.0) - half_width + (hand_cards[0].texture.get_width() / 2.0)
+	$StartX.position = Vector2(start_x, 40)
+	
+	
+	# var start_x = (get_viewport_rect().size.x / 2.0) - ((card_num / 2.0) * (hand_cards[0].texture.get_width() + card_spacing))
 	var y = get_viewport_rect().size.y + y_offset
 	
 	var tween : Tween = create_tween()
 	tween.set_parallel()
 	
-	for i in get_child_count():
-		var card : Card = get_child(i)
+	for i in card_num:
+		var card : Card = hand_cards[i]
 		
-		var x : int = start_x + (i * card_spacing)
+		var x : int = start_x + (i * (card_spacing + hand_cards[0].texture.get_width()))
 		var target_position : Vector2 = Vector2(x, y)
-		tween.tween_property(card, "position", target_position, 1.0)
+		tween.set_trans(Tween.TRANS_CIRC)
+		tween.tween_property(card, "position", target_position, 0.6)
 	
 	tween.play()
 
-func highlight_card() -> void:
-	pass
+
