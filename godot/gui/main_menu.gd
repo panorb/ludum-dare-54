c16b01b5864161da825ae5d61f88d6f0f7df9858
Paul Norberger <panor@fastmail.com>
2023-10-01 06:49:21 +0200

    Refactor main_menu.gd to follow style conventions more closely

--- godot/gui/main_menu.gd
+++ godot/gui/main_menu.gd
@@ -1,34 +1,39 @@
 extends Node
 
+
+signal start_game
+signal show_credits
+
+@export var start_volume : float = 70
+
 @onready var start_button : BaseButton = %StartButton
 @onready var sound_slider : HSlider = %SoundSlider
 @onready var sound_percent_label : Label = %SoundPercentLabel
 @onready var credits_button : BaseButton = %CreditsButton
 @onready var quite_button : BaseButton = %QuitButton 
-
 @onready var master_bus_index : int = AudioServer.get_bus_index('Master')
 
-@export var start_volume : float = 70
 
-signal start_game
-signal show_credits
-
-func _ready():
-	start_button.pressed.connect(self._start_button_pressed)
-	quite_button.pressed.connect(self._quit_button_pressed)
-	sound_slider.value_changed.connect(self._sound_value_changed)
+func _ready() -> void:
+	start_button.pressed.connect(self._on_StartButton_pressed)
+	quite_button.pressed.connect(self._on_QuitButton_pressed)
+	sound_slider.value_changed.connect(self._on_SoundSlider_value_changed)
 	
 	sound_slider.value = self.start_volume
 
-func _start_button_pressed():
+
+func _on_StartButton_pressed() -> void:
 	self.start_game.emit()
 
-func _credits_button_pressed():
+
+func _on_CreditsButton_pressed() -> void:
 	self.show_credits.emit()
 
-func _quit_button_pressed():
+
+func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
 
-func _sound_value_changed(volume_value: float):
+
+func _on_SoundSlider_value_changed(volume_value: float) -> void:
 	AudioServer.set_bus_volume_db (master_bus_index, volume_value)
 	sound_percent_label.text = str(volume_value) +"%"
