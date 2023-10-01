129a4915878e6b2e5c4b263cc0266a42e552ca4f
Luka-sama <luka-sama@pm.me>
2023-10-01 11:37:58 +0200

    Delete world node when started

--- godot/scene_manager.gd
+++ godot/scene_manager.gd
@@ -6,21 +6,22 @@ var packed_scenes := {
 var start_scene := "game";
 
 var _scenes := {};
-var _curr_scene: Node = null;
-@onready var _world = get_node("/root/World");
+@onready var _curr_scene: Node = get_node_or_null("/root/World");
+@onready var _root = get_node("/root");
 
 func _ready() -> void:
 	_init_scenes()
-	show_scene(start_scene)
+	if _curr_scene != null:
+		show_scene(start_scene)
 
 func show_scene(scene_name: String) -> void:
 	var scene: Node = _scenes[scene_name]
 	if scene == _curr_scene:
 		return
 	if _curr_scene != null:
-		_world.remove_child(_curr_scene)
+		_root.remove_child.call_deferred(_curr_scene)
 	_curr_scene = scene
-	_world.add_child(scene)
+	_root.add_child.call_deferred(scene)
 
 func _init_scenes() -> void:
 	for scene_name in packed_scenes:
