a62fffd01ad82dcc17637dbc8fb5360cedff76ae
Julian Pape-Lange <j.pape-lange@gmx.de>
2023-10-01 17:25:42 +0200

    Add decorations

--- godot/building/building.gd
+++ godot/building/building.gd
@@ -6,11 +6,18 @@ class_name Building extends Node
 @export var max_width : int = 10
 @export var floor_width_distribution = [0, 0, 0.1, 0.25, 0.4, 0.55, 0.7, 0.8, 0.9, 0.96, 1]
 @export var floor_number_distribution = [0, 0.3, 0.4, 1]
-@export var border_probability = 0.3
+@export var border_probability = 1
 @export var border_skew_probability = 0.5
-@export var adjusted_expanding_distribution = [0.05, 0.22, 0.68, 0.98, 1]
+@export var adjusted_expanding_distribution = [0.05, 0.22, 0.28, 0.98, 1]
 @export var no_tshape_probability = 0.2
+
 @export var border_window_probability = 0.4
+@export var hole_probability = 0.1
+@export var trap_door_probability = 0.25
+
+@export var window_probability = 0.4
+@export var roof_probability = 0.7
+
 
 var grid = []
 var size = Vector2i.ZERO
@@ -80,11 +87,11 @@ func generate_building() -> void:
 						if i+j == -shrink_size - 1:
 							grid[floor_height*floor_number-1-i][max_width-1-j].set_slope(4)
 	
-	generate_decorations()
+	update_edges()
 	
 	generate_windows()
-	
-	update_edges()
+	generate_trap_doors()
+	generate_decorations()
 
 func randomize_floor_number() -> int:
 	var random := randf()
@@ -164,7 +171,34 @@ func initialize_grid() -> void:
 			grid[i].append(tile_instance)
 
 func generate_decorations() -> void:
-	pass
+	if randf() < roof_probability:
+		for j in size.x:
+			if not grid[0][j].is_empty or grid[0][j].slope:
+				grid[0][j].set_decoration(3)
+	
+	for i in size.y:
+		for j in size.x:
+			if grid[i][j].decoration:
+				continue
+			if allows_window(i, j) and randf() < window_probability:
+				grid[i][j].set_decoration(1)
+			if allows_hole(i, j) and randf() < hole_probability:
+				grid[i][j].set_decoration(2)
+
+func allows_window(i, j) -> bool:
+	if grid[i][j].is_empty:
+		return false
+	if i%floor_height != 1:
+		return false
+	if grid[i][j].left_right_edge:
+		return false
+	
+	return true
+
+func allows_hole(i, j) -> bool:
+	if grid[i][j].is_decorated or grid[i][j].is_empty:
+		return false
+	return true
 
 func generate_windows() -> void:
 	if not is_left_border and not is_right_border:
@@ -175,7 +209,26 @@ func generate_windows() -> void:
 		j = size.x - 1
 	
 	for i in size.y:
+		if grid[i][j].decoration or grid[i][j].is_empty:
+			continue
 		if i%floor_height == 1:
 			if randf() < border_window_probability:
 				grid[i][j].set_window()
+
+func generate_trap_doors() -> void:
+	for j in size.x:
+		if grid[0][j].is_decorated or grid[0][j].is_empty:
+			continue
+		if j > 0 and grid[0][j-1].is_trap_door:
+			continue
+		if randf() < trap_door_probability:
+			grid[0][j].set_trap_door()
+	
+	for j in size.x:
+		if grid[size.y-1][j].is_decorated or grid[size.y-1][j].is_empty:
+			continue
+		if j > 0 and grid[0][j-1].is_trap_door:
+			continue
+		if randf() < trap_door_probability:
+			grid[size.y-1][j].set_trap_door()
 	
--- godot/building/building_tile.gd
+++ godot/building/building_tile.gd
@@ -4,10 +4,14 @@ var is_empty = true
 var is_top = false
 var is_bottom = false
 var is_window = false
+var is_trap_door = false
+var is_door
 var slope = 0
 var left_right_edge = 0
 var tile = Vector2i(-1, -1)
 var tile_decoration = Vector2i(-1, -1)
+var decoration = 0
+var is_decorated = false
 
 func set_brick():
 	is_empty = false
@@ -41,6 +45,22 @@ func set_bottom() -> void:
 
 func set_window() -> void:
 	is_window = true
+	is_decorated = true
+	update_tile()
+
+func set_trap_door() -> void:
+	is_trap_door = true
+	is_decorated = true
+	update_tile()
+
+func set_decoration(number) -> void:
+	decoration = number
+	if number:
+		is_decorated = true
+	# 1 = window
+	# 2 = hole
+	# 3 = roof
+	update_tile()
 
 func update_tile():
 	if not self.is_empty:
@@ -77,8 +97,30 @@ func update_tile():
 			elif self.slope == 4:
 				tile = Vector2i(6, 3)
 	
+	if decoration:
+		if decoration == 1:
+			tile_decoration = Vector2i(1, 2)
+		if decoration == 2:
+			tile_decoration = Vector2i(3, 5)
+		if decoration == 3:
+			if not is_empty:
+				tile_decoration = Vector2i(1, 0)
+			elif slope == 1:
+				tile_decoration = Vector2i(0, 0)
+			else:
+				tile_decoration = Vector2i(2, 0)
+
 	if is_window:
 		if left_right_edge == 1:
 			tile_decoration = Vector2i(0, 2)
 		elif left_right_edge == 2:
 			tile_decoration = Vector2i(2, 2)
+	
+	if is_trap_door:
+		if is_top:
+			if decoration == 3:
+				tile_decoration = Vector2i(3, 3)
+			else:
+				tile_decoration = Vector2i(4, 4)
+		elif is_bottom:
+			tile_decoration = Vector2i(3, 4)
