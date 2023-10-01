7331573fc8bce186280d9bcea08a0147dbeb0520
Luka-sama <luka-sama@pm.me>
2023-10-01 17:59:27 +0200

    Prevent camera moving together with letter scrolling

--- godot/game/InteractionManager.gd
+++ godot/game/InteractionManager.gd
@@ -3,25 +3,27 @@ extends Node2D
 @export var camera: Camera2D
 @export var camera_sensitivity: float = 5.0
 @export var drag_start_time: int = 150
+@onready var _letter := get_node("%Letter")
 
 signal primary_interaction_just_pressed_sig
 
 const MOUSE_SCROLL_SPEED: int = 10
 
 func _process(delta: float) -> void:
-
+	if _letter.visible:
+		return
 
 	var movement = 0;
 
 	if Input.is_action_pressed("camera_up"):
 		movement -= 1
-	
+
 	if Input.is_action_pressed("camera_down"):
 		movement += 1
 
 	if Input.is_action_just_released("camera_wheel_up"):
 		movement -= MOUSE_SCROLL_SPEED
-	
+
 	if Input.is_action_just_released("camera_wheel_down"):
 		movement += MOUSE_SCROLL_SPEED
 
@@ -40,6 +42,9 @@ var current_state = MOUSE_STATE.INACTIVE
 var starting_position = null
 var primary_pressed = false
 func _input(event: InputEvent) -> void:
+	if _letter.visible:
+		return
+
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("primary_action"):
 			primary_pressed = true
@@ -51,7 +56,7 @@ func _input(event: InputEvent) -> void:
 			current_state = MOUSE_STATE.CLICKING
 			starting_position = event.position
 		elif current_state == MOUSE_STATE.CLICKING:
-			if starting_position != null: 
+			if starting_position != null:
 				if starting_position.distance_to(event.position) > 5:
 					current_state = MOUSE_STATE.DRAGGING
 			else:
