b9984a8fabaa3f2a353d4b85c5bc62d788b0d630
Paul Norberger <panor@fastmail.com>
2023-10-02 12:18:32 +0200

    More work on capacity ui

---
+++ godot/gui/capacity/capacity_ui.gd
@@ -0,0 +1,11 @@
+extends Control
+
+var population_tween : Tween
+var capacity_tween : Tween
+var _displayed_population: int = 0
+var _displayed_capacity: int = 0
+var population: int = 0:
+	set(value):
+		
+var capacity: int = 0
+
